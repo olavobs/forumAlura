3ba664ba1f869717bfab48c70b2fa731c123adf1
Olavo Souza <olavo.briso@gmail.com>
2020-08-27 10:48:25 -0300

    Add autentication and authorization configs

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$P.3VEXulEkxKDl775HOi9u1P9oqpMOTToM0kZ5Nk.qaNDtltFy5Pq');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
